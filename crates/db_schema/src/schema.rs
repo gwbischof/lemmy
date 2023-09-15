4e203e30f83e983f33f2a274cfa1800e45304766
Garrett Bischof <bischofgarrett@gmail.com>
2023-09-14 23:11:11 -0400

    update schema

--- crates/db_schema/src/schema.rs
+++ crates/db_schema/src/schema.rs
@@ -95,6 +95,7 @@ diesel::table! {
         path -> Ltree,
         distinguished -> Bool,
         language_id -> Int4,
+        bid -> Int4
     }
 }
 
@@ -734,6 +735,14 @@ diesel::table! {
         featured_local -> Bool,
         url_content_type -> Nullable<Text>,
         alt_text -> Nullable<Text>,
+        pickup_location -> Nullable<Text>,
+        pickup_time -> Nullable<Timestamptz>,
+        pickup_contact -> Nullable<Text>,
+        pickup_notes -> Nullable<Text>,
+        dropoff_location -> Nullable<Text>,
+        dropoff_time -> Nullable<Timestamptz>,
+        dropoff_contact -> Nullable<Text>,
+        dropoff_notes -> Nullable<Text>,
     }
 }
 
