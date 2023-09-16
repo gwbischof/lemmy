6b76e1cf33899bef311e83220317a8a2375c7aa1
Garrett Bischof <bischofgarrett@gmail.com>
2023-09-16 19:13:24 -0400

    post_move_request migration

---
+++ migrations/2023-09-16-230213_post_move_request/down.sql
@@ -0,0 +1,8 @@
+alter table post DROP pickup_location text;
+alter table post DROP pickup_time timestamp;
+alter table post DROP pickup_contact text;
+alter table post DROP pickup_notes text;
+alter table post DROP dropoff_location text;
+alter table post DROP dropoff_time timestamp;
+alter table post DROP dropoff_contact text;
+alter table post DROP dropoff_notes text;
