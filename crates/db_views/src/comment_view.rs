bd975a6907b7c06dab3b6ed835162c2ec5559f43
Garrett Bischof <bischofgarrett@gmail.com>
2023-09-16 16:28:38 -0400

    update post and comment views

--- crates/db_views/src/comment_view.rs
+++ crates/db_views/src/comment_view.rs
@@ -967,6 +967,7 @@ mod tests {
         distinguished: false,
         path: data.inserted_comment_0.clone().path,
         language_id: LanguageId(37),
+        bid: None,
       },
       creator: Person {
         id: data.timmy_local_user_view.person.id,
@@ -1013,6 +1014,14 @@ mod tests {
         language_id: Default::default(),
         featured_community: false,
         featured_local: false,
+        pickup_location: None,
+        pickup_time: None,
+        pickup_contact: None,
+        pickup_notes: None,
+        dropoff_location: None,
+        dropoff_time: None,
+        dropoff_contact: None,
+        dropoff_notes: None,
       },
       community: Community {
         id: data.inserted_community.id,
